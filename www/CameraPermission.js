f4ad58e754ead99d137b109890988ea9a5ed6d0a
Chihpin D <chihpindu@gmail.com>
2018-09-11 14:53:58 +0800

    bug fixed
    bug fixed

--- www/CameraPermission.js
+++ www/CameraPermission.js
@@ -2,13 +2,13 @@
  * A simple plugin for Cordova to check auth to the camera on iOS.
  */
 
-var cameraPermission = function() {};
+var CameraPermission = function() {};
 
 /*
 *	success - success callback
 *	fail - error callback
 */
-cameraPermission.prototype.checkAuthorization = function(success, fail) {
+CameraPermission.prototype.checkAuthorization = function(success, fail) {
   if (device.platform.toLowerCase() != 'ios') {
     success();
     return;
@@ -16,4 +16,4 @@ cameraPermission.prototype.checkAuthorization = function(success, fail) {
   cordova.exec(success, fail, "CameraPermission", "checkAuthorization");
 };
 
-module.exports = new cameraPermission();
+module.exports = new CameraPermission();
